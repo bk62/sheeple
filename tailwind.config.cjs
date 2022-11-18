db9cafd08580e2b64a7b2749c328654a3c45d909
bk62 <bkafle862@gmail.com>
2022-11-18 11:53:05 +0545

    update styles

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,46 @@
 /** @type {import('tailwindcss').Config} */
+require("tailwindcss/colors");
+
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "dark-gray": "#1c1c28",
+        "mid-gray": "#28293d",
+        "light-gray": "#8f90a6",
+
+        "light-blue": "#9dbff9",
+        "mid-blue": "#5b8def",
+        "dark-blue": "#0063f7",
+
+        "light-orange": "#fccc75",
+        "mid-orange": "#fdac42",
+        "dark-orange": "#ff8800",
+        
+        "light-purple": "#dda5e9",
+        "mid-purple": "#ac5dd9",
+        "dark-purple": "#6600cc",
+
+        "brand": "#6b64f2"
+
+      },
+      fontSize: {
+        xss: "0.6rem"
+      },
+      gridTemplateColumns: {
+        "dashboard": "180px 1fr"
+      },
+      gridTemplateRows: {
+        "dashboard": "50px auto 1fr 50px",
+        "dashboard-md": "50px 1fr 50px"
+      }
+    },
   },
-  plugins: [],
+  plugins: [
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/line-clamp"),
+    require("@tailwindcss/aspect-ratio")
+  ],
 };
