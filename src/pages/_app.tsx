8f476b2c794359d2c1f58abba3578045bad9d069
bk62 <bkafle862@gmail.com>
2022-11-18 13:09:52 +0545

    Font, layout and icons

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,19 @@
 // import { type AppType } from "next/app";
+import Head from "next/head";
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 import type { AppProps } from "next/app";
 
+import { Inter } from "@next/font/google";
+const inter = Inter();
+
+import "@fortawesome/fontawesome-svg-core/styles.css";
+import { config } from "@fortawesome/fontawesome-svg-core";
+config.autoAddCss = false;
+
 import { trpc } from "../utils/trpc";
 
-import PrimaryLayout, { getPrimaryLayout } from "../components/layouts/primary";
+import { getPrimaryLayout } from "../components/layouts/primary";
 import "../styles/globals.css";
 import type { NextPageWithLayout } from "./page";
 
@@ -24,6 +32,18 @@ const MyApp = ({ Component, pageProps: { session, ...pageProps } }: AppPropsWIth
   const getLayout = Component.getLayout ?? getPrimaryLayout;
   return (
     <SessionProvider session={session}>
+      <Head>
+        <title>sheeple</title>
+        <link rel="icon" href="/favicon.ico" />
+        <style>
+          {`
+            body {
+              font-family: ${inter.style.fontFamily}, sans-serif;
+              font-feature-settings: kern;
+            }
+          `}
+        </style>
+      </Head>
       {getLayout(<Component {...pageProps} />)}
     </SessionProvider>
   );
